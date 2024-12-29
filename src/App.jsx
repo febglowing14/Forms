d2ed72c0223a115e95ff65577a3663ebccba741d
febglowing14 <gopikavijayarajan@gmail.com>
2024-12-30 01:17:26 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,56 @@
 
 import react from 'react';
- export default function App() {
+export default function App() {
 
   function signup(formData) {
-    
-   const email= formData.get('email');
-   const password= formData.get('password');
-   const description= formData.get('description');
-   console.log(email)
-   console.log(password)
-   console.log(description)
-   
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const description = formData.get('description');
+    console.log(email)
+    console.log(password)
+    console.log(description)
+
   }
-  return(
+  return (
     <section>
       <h3>Signup Form</h3>
 
-  <form action={signup}>
-  
-    <label>Email: </label>
-    <input name='email' placeholder="abcd@gmail.com"/>
-    <br/>
-    <br />
-    <label>Password:</label>
-    <input name='password' placeholder='password'/>
-    <br/>
-    <br/>
-    <label>Description:</label>
-    <textarea name='description' placeholder='description'/>
-    <fieldset>
-      <legend>Employment  status</legend>
-    
-    <label>
-      <input type='radio' name='employment' value='employed'/>
-     unemployed
-    </label>
-    <label>
+      <form action={signup}>
+
+        <label>Email: </label>
+        <input name='email' placeholder="abcd@gmail.com" />
+        <br />
+        <br />
+        <label>Password:</label>
+        <input name='password' placeholder='password' />
+        <br />
+        <br />
+        <label className='ds'>Description:</label>
+        <br />
+        <textarea className="ta" name='description' placeholder='description' />
+        <fieldset className='fs'>
+          <legend>Employment  status</legend>
+
+          <label>
+            <input type='radio' name='employment' value='employed' />
+            unemployed
+          </label>
+          <br/>
+          <label>
             <input type="radio" name="employmentStatus" />
             Part-time
-        </label>
+          </label>
+          <br/>
           <label>
             <input type="radio" name="employmentStatus" />
             Full-time
-        </label>
-</fieldset>
-    <button>submit</button>
+          </label>
+        </fieldset>
+
+        <button>submit</button>
       </form>
-  </section>
+    </section>
   )
 }
 
@@ -58,4 +62,4 @@ import react from 'react';
 
 
 
- 
+
